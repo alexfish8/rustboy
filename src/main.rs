90f9db8d75ccb7b82bde6de75770baeead2f6cb0
alexfish8 <alexfish8@gmail.com>
2017-02-27 15:47:07 -0800

    Remove LICENSE file; separate code into separate files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,46 @@
+extern crate rustboy;
+
+use rustboy::cartridge::Cartridge;
+use rustboy::gameboy::Gameboy;
+
+use std::env;
+use std::process;
+use std::fs::File;
+
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut args = env::args();
+    args.next();
+
+    let rom_path = match args.next() {
+        Some(arg) => arg,
+        None      => {
+            println!("Need to specify a valid ROM path!");
+            process::exit(1);
+        }
+    };
+
+    let mut f = match File::open(&rom_path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Failed to open file: {}", rom_path);
+            process::exit(1);
+        }
+    };
+
+    let cartridge = match Cartridge::new(&mut f) {
+        Ok(c)    => c,
+        Err(msg) => {
+            println!("Failed to load cartridge: {}", msg);
+            process::exit(1);
+        }
+    };
+
+    //
+
+    println!("Got rom path: {}", rom_path);
+
+    Gameboy::run(&cartridge);
+
 }
